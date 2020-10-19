29f440bfeb0c8e6e1023b8274bedbaad2ef236d0
Your Name <61206200+Golamrabbani-ux@users.noreply.github.com>
2020-10-19 04:07:04 -0500

    search query add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
 
 
     useEffect(()=>{
-        fetch(`http://localhost:4000/search?title=${title.title}`)
+        fetch(`https://volunteer-network-server-gr.herokuapp.com/search?title=${title.title}`)
         .then(res => res.json())
         .then(data => {
             setSearchData(data);
